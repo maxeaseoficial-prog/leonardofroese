49feab6532bb17971e475b831489afd3608b62ea
Henrique Castro <maxeaseoficial@gmail.com>
2026-09-17 14:39:07 -0300

    fix: remove duplicate client logos from raio-x result

--- src/routes/raiox/resultado.tsx
+++ src/routes/raiox/resultado.tsx
@@ -12,7 +12,6 @@ import {
 import { funnelHead } from "@/features/lucro-2x/funnel-head";
 import { FunnelShell } from "@/features/lucro-2x/funnel-ui";
 import { LiveExperience } from "@/features/lucro-2x/live-experience";
-import { ClientLogos } from "@/components/site/client-logos";
 
 export const Route = createFileRoute("/raiox/resultado")({
   head: () => funnelHead,
@@ -120,7 +119,6 @@ function Result() {
         </section>
       </article>
 
-      <ClientLogos className="pb-20 pt-10" />
       <LiveExperience />
     </FunnelShell>
   );
